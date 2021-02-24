f427ecdc59fde32102159c9f02519b18219d8434
Yordi <yordi@advansales.com>
2021-02-24 17:13:39 -0600

    nuevos servicios

--- routes/cita.js
+++ routes/cita.js
@@ -16,7 +16,7 @@ router.get('/:tipo', validarJWT, usuarioCitas)
 router.get('/paciente/:tipo', validarJWT, citaPaciente)
 router.get('/medico/:citaId', validarJWT, aceptarCitaMedico)
 router.get('/medicocita:tipo', validarJWT, citasMedico)
-router.get('/medicocitasolicitud/:tipo&:estado', validarJWT, citasMedicoSolicitud)
+router.get('/medicocitasolicitud/:tipo/:estado', validarJWT, citasMedicoSolicitud)
 router.get('/detalle/:citaId', validarJWT, getCita)
 
 module.exports = router;
